a085054c477ae54505742571e33e1e79465030c3
Kumar Sanu <kumsanu@WKMZTE07F168.global.publicisgroupe.net>
2023-02-01 13:36:51 +0530

    added dropzone

---
+++ components/Header/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "20px",
+        fontSize: "24px",
+      }}
+    >
+      <div>
+        {/* <img>Logo</img> */}
+        <div>Tools</div>
+      </div>
+      <div>User</div>
+    </div>
+  );
+};
+
+export default Header;
